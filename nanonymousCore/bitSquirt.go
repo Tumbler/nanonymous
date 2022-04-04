f21919be77d31c41a4c22df2affb6adda92bf824
Tumbler <TumblerTerrall@gmail.com>
2022-04-04 16:32:12 -0500

    Refactored bitSquirt into its own file.
    Moved entropy function back into main file
    Removed global declaration of currentEntropy
    BitSquirt now is just a struct and member functions
    You now need to declare a new bitSquirt variable when you want to use one
    Remved newBitSquirt function (use bitSlurp to fill the byte array)

---
+++ nanonymousCore/bitSquirt.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+   "math"
+)
+
+type bitSquirt struct {
+   bits []byte
+   currentBit int
+   maxBits int
+}
+
+func (storedData *bitSquirt) newBitSquirt(newBits []byte) *bitSquirt {
+   newBS := bitSquirt{newBits, 0, 0}
+   newBS.maxBits = len(newBS.bits) * 8
+   return &newBS
+}
+
+// TODO possible memory leak??
+func (storedData *bitSquirt) resetBitSquirt() {
+   storedData.bits = make([]byte, 0)
+   storedData.currentBit = 0;
+   storedData.maxBits = 0;
+}
+
+func (storedData *bitSquirt) restartBitSquirt() {
+   storedData.currentBit = 0;
+}
+
+func (storedData *bitSquirt) squirtBits(numBits int) int {
+   var bits int
+   var tmp byte
+
+   for i := 0; i < numBits; i++ {
+      index := storedData.currentBit / 8
+      mask  := byte(math.Pow(float64(2), float64(8 - (storedData.currentBit % 8 + 1))))
+      tmp = storedData.bits[index] & mask
+
+      bits = bits << 1
+      if (tmp >= 1) {
+         bits |= 1
+      }
+
+      storedData.currentBit++
+
+      if (storedData.currentBit > storedData.maxBits) {
+         storedData.currentBit = 0
+         break
+      }
+   }
+   // TODO what if maxbits isn't divisible by 8?
+
+   return bits
+}
+
+func (storedData *bitSquirt) slurpBits(bitSoup int64, numBits int) {
+   var tmp int64
+   for i := 0; i < numBits; i++ {
+      index := storedData.currentBit / 8
+      mask  := (int64)(1 << (numBits - 1 - i))
+      tmp = bitSoup & mask
+
+      // Add additional storage space if we need
+      if (len(storedData.bits) <= index) {
+         storedData.bits = append(storedData.bits, 0)
+      }
+
+      storedData.bits[index] = storedData.bits[index] << 1
+      if (tmp >= 1) {
+         storedData.bits[index] |= 1
+      }
+
+      storedData.currentBit++
+   }
+
+   storedData.maxBits += numBits
+}
+
+func (storedData *bitSquirt) getBitSquirtLength() int {
+   if (storedData.maxBits > 0) {
+      return len(storedData.bits)
+   } else {
+      return 0
+   }
+}
+
+// TODO make squirtbits return value so can evaulate if done with bit
