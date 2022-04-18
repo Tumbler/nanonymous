06f824492795ca393f1bb3d6c757a4a24f1758df
Tumbler Terrall <TumblerTerrall@gmail.com>
2022-04-18 16:27:33 -0500

    Added sample code for using pgx to connect to postgres
    Also renamed bip39-English.txt because case-sensitive lol.

--- nanonymousCore/core.go
+++ nanonymousCore/core.go
@@ -3,7 +3,14 @@ package main
 import (
    "fmt"
    "time"
+   "context"
+   "encoding/hex"
+
+   // Local packages
    keyMan "nanoKeyManager"
+
+   // 3rd party packages
+   pgx "github.com/jackc/pgx/v4"
 )
 
 func main() {
@@ -12,27 +19,106 @@ func main() {
    var usr string
    var seed keyMan.Key
 
-   menu:
-   for {
+   //menu:
+   //for {
       fmt.Print("1. Generate Seed\n",
-                "2. Input Mnomonic\n",
-                "3. Get next address\n",
-                "4. Delete stored seed\n",
-                "5. Test stuff\n",
-                "6. Nano address to pubkey\n")
+                "2. Database test\n",
+                "3. Insert into database\n")
       fmt.Scan(&usr)
 
-      keyMan.WalletVerbose(true)
       switch (usr) {
       case "1":
+         keyMan.WalletVerbose(true)
+
          err := keyMan.GenerateSeed(&seed)
          if (err != nil){
             fmt.Println(err.Error())
          }
 
+         keyMan.WalletVerbose(false)
+      case "2":
+         var databaseUrl = "postgres://postgres:folliCle99@localhost:5432/postgres"
+
+         conn, err := pgx.Connect(context.Background(), databaseUrl)
+
+         if (err != nil) {
+            fmt.Println("main: ", err)
+            return
+         }
+         defer conn.Close(context.Background())
+
+         var id int
+         var name string
+         var number string
+
+         rows, err := conn.Query(context.Background(), "SELECT * FROM gotest1")
+
+         if (err != nil) {
+            fmt.Println("QueryRow failed: ", err)
+            return
+         }
+
+         for rows.Next() {
+            err = rows.Scan(&id, &name, &number)
+            if (err != nil) {
+               fmt.Println("Scan failed: ", err)
+               return
+            }
+            fmt.Println("ID: ", id, "Name: ", name, "Number: ", number)
+         }
+
+      case "3":
+         var databaseUrl = "postgres://postgres:folliCle99@localhost:5432/nanonymousdb"
+         var newSeed keyMan.Key
+
+         conn, err := pgx.Connect(context.Background(), databaseUrl)
+
+         if (err != nil) {
+            fmt.Println("main: ", err)
+            return
+         }
+         defer conn.Close(context.Background())
+
+         err = keyMan.GenerateSeed(&newSeed)
+         if (err != nil) {
+            fmt.Println("main: ", err)
+            break
+         }
+
+         hexString := hex.EncodeToString(newSeed.Seed)
+         fmt.Println("seed: ", hexString)
+
+         err = insertSeed(conn, newSeed.Seed)
+
+         if (err != nil) {
+            fmt.Println("main: ", err)
+            break //menu
+         }
+
       default:
-         break menu
+         break //menu
       }
-      keyMan.WalletVerbose(false)
+   //}
+
+
+}
+
+
+func insertSeed(conn *pgx.Conn, seed []byte) error {
+   queryString :=
+   "INSERT INTO " +
+     "seeds (seed, currentIndex) " +
+   "VALUES " +
+     "($1, 0);"
+
+   rowsAffected, err := conn.Exec(context.Background(), queryString, seed)
+   if (err != nil) {
+      return fmt.Errorf("insertSeed: %w", err)
    }
+
+   if (rowsAffected.RowsAffected() < 1) {
+      return fmt.Errorf("insertSeed: no rows affected")
+   }
+
+   return nil
 }
--- nanonymousCore/go.mod
+++ nanonymousCore/go.mod
@@ -6,8 +6,17 @@ require nanoKeyManager v1.0.0
 
 require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.11.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.10.0 // indirect
+	github.com/jackc/pgx/v4 v4.15.0 // indirect
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/text v0.3.6 // indirect
 )
 
 replace nanoKeyManager => ./nanoKeyManager
