1ba29ac7525d2b45c617b55f7f1cc1ef2483e7af
Tumber Terrall <TumblerTerrall@gmail.com>
2023-03-30 14:54:51 +0000

    Updated php to use secure connections to core.

--- nanonymousFrontEnd/php/getFinalHash.php
+++ nanonymousFrontEnd/php/getFinalHash.php
@@ -1,7 +1,12 @@
 <?php
 $finalAddress=$_GET['address'];
 
-$socket = fsockopen("147.182.231.89", 41721, $errno, $errstr, 30);
+$context = stream_context_create(
+   ['ssl' => [
+     'allow_self_signed' => true
+]]);
+
+$socket = stream_socket_client('ssl://147.182.231.89:41721', $errno, $errstr, 30, STREAM_CLIENT_CONNECT, $context);
 if (!$socket) {
    echo "comm error: $errstr ($errno)<br />\n";
 } else {
