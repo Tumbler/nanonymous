f0af77c724b1931a4fcf0a7c5830a2071ae02b8c
Tumbler Terrall <TumblerTerrall@gmail.com>
2023-09-14 21:26:01 -0500

    Changed dir name back from archive to backup.

--- scripts/backupdatabase.sh
+++ scripts/backupdatabase.sh
@@ -1,7 +1,7 @@
 _now=$(date +"%Y_%m_%d")
-_file="./archive/nanoymousdb_$_now.dump"
+_file="./backup/nanoymousdb_$_now.dump"
 
 pg_dump -U postgres -Fc nanonymousdb > "$_file"
 
 # Delete everything older than a month but still keep one a month
-find ./archive/* -type f -not -path './archive/*01.dump' -mtime +30 -exec rm {} \;
+find ./backup/* -type f -not -path './backup/*01.dump' -mtime +30 -exec rm {} \;
