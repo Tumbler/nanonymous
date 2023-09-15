bae55d8b131b27c5f260fa8f8ba0b9c9ff6b7b14
Tumbler Terrall <TumblerTerrall@gmail.com>
2023-09-14 22:01:33 -0500

    Made some changes after actually testing backup script.

--- scripts/backupdatabase.sh
+++ scripts/backupdatabase.sh
@@ -1,7 +1,7 @@
 _now=$(date +"%Hh-%Y_%m_%d")
-_file="./backup/nanoymousdb_$_now.dump"
+_file="./backups/nanoymousdb_$_now.dump"
 
-pg_dump -U postgres -Fc nanonymousdb > "$_file"
+pg_dump -U go -Fc nanonymousdb > "$_file"
 
 # Delete everything older than a month but still keep one a month
-find ./backup/* -type f -not -path './backup/*01.dump' -mtime +30 -exec rm {} \;
+find ./backups/* -type f -not -path './backups/*01.dump' -mtime +30 -exec rm {} \;
