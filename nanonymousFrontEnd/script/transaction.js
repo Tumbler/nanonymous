5991eda9c58c068f6f533d32fe9d29dec0b2723d
Tumber Terrall <TumblerTerrall@gmail.com>
2023-09-20 15:56:41 +0000

    QR code now has a tooltip that says "Tap me!" on mobile devices.
    The fee calculator is now labeled so people know it's just a calculator and not
       part of the transaction.
    The QR code now updates live if you change the calculator after requesting an
       address.

--- nanonymousFrontEnd/script/transaction.js
+++ nanonymousFrontEnd/script/transaction.js
@@ -1,9 +1,24 @@
-let nanonymousFee = 0.01
+let nanonymousFee = 0.01;
+let QRactive = false;
+let middleAddress = "";
+
+let mobileOrTablet = mobileOrTabletCheck()
 
 function showQR() {
    var finalAddress = document.getElementById("finalAddress").value;
    document.getElementById("errorMessage").innerHTML = "";
    ajaxGetAddress(finalAddress);
+
+   if (mobileOrTablet) {
+      var tooltip = document.getElementById("tooltiptap");
+
+      setTimeout(function(){
+         tooltip.style.opacity = '1';
+         setTimeout(function(){
+            tooltip.style.opacity = '0';
+         }, 3000);
+      }, 1500);
+   }
 }
 
 function autoFill(caller) {
@@ -32,6 +47,24 @@ function autoFill(caller) {
          document.getElementById("USDamount").value = usd;
          document.getElementById("nanoAmount").value = nano;
          break;
+   }
+
+   if (QRactive) {
+      let Nano = document.getElementById("afterTaxAmount").value;
+      let raw = nanocurrency.convert(Nano, {from:"Nano", to:"raw"})
+      var qrCodeText = "nano:" + middleAddress + "?amount=" + raw;
+      if (qrCodeText.length < 85) {
+         var qrSize = 250;
+      } else {
+         var qrSize = 275;
+      }
+
+      document.getElementById("QRLink").href = qrCodeText;
+
+      var qr = new QRious({
+         element: document.getElementById("QRCode"),
+         foreground: '#151515', size: qrSize, padding: 5, value: qrCodeText
+      });
    }
 }
 
@@ -162,10 +195,8 @@ async function ajaxGetAddress(finalAddress) {
       console.log(this.response);
       var reply = this.response.match(/address=(nano_[a-z0-9]+)/i);
       var info = this.response.match(/info=(.*)\n$/i);
-      let middleAddress = "";
       if (reply !== null && reply.length > 1) {
          middleAddress = reply[1];
-         document.getElementById("TransactionInfo").innerHTML = "Tap QR code to open wallet if on mobile"
 
          var qrCodeText = "nano:" + middleAddress + "?amount=" + raw;
 
@@ -185,6 +216,7 @@ async function ajaxGetAddress(finalAddress) {
          document.getElementById("button").style.display = "none";
          document.getElementById("scanQR").hidden = true;
          setTimeout(window.scrollTo(0, document.body.scrollHeight),100);
+         QRactive = true;
       } else if (info !== null && info.length > 1) {
          document.getElementById("errorMessage").innerHTML = info[1];
          document.getElementById("errorMessage").scrollIntoView();
@@ -255,22 +287,14 @@ async function ajaxGetAddress(finalAddress) {
             document.getElementById("qr-label").classList.remove("animate-fade-in");
             document.getElementById("qr-label").classList.add("animate-zipRight-out");
             setTimeout(function(){ // delay by 100 ms
-            document.getElementById("TransactionInfo").classList.add("animate-zipRight-out");
             document.getElementById("QRdiv").style.maxHeight = "0px";
 
             document.getElementById("HashLink").href = "https://www.nanolooker.com/block/" + hash;
             document.getElementById("HashLink").innerHTML = "Final hash:<br>" + hash;
             document.getElementById("HashLink").style.color = "#313133";
             document.getElementById("Hashdiv").style.maxHeight = "1000px";
+
             setTimeout(function(){ // delay by 900 ms
-            document.getElementById("TransactionInfo").classList.remove("animate-zipRight-out");
-            document.getElementById("TransactionInfo").style.textAlign = "center";
-            document.getElementById("TransactionInfo").innerHTML = "Transaction Complete!"
-            document.getElementById("TransactionInfo").classList.add("animate-zipRight-in");
-
-            setTimeout(function(){ // delay by 100 ms
-
-
             document.getElementById("Hashdiv").classList.add("animate-zipRight-in");
             document.getElementById("HashLink").style.removeProperty("color");
 
@@ -306,12 +330,12 @@ async function ajaxGetAddress(finalAddress) {
                document.body.removeChild(confettiCanvas);
             }, 5000);
             }, 1000);
-            }, 100);
             }, 900);
             }, 100);
             }, 100);
             }, 100);
 
+            QRactive = true;
             req2.abort();
          } else {
             console.log(this.response);
@@ -416,3 +440,9 @@ function copyAddress() {
 
    setTimeout(function(){tooltip.style.opacity = '0';}, 3000);
 }
+
+function mobileOrTabletCheck() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
